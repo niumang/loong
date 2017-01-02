1835e5feebd8717617fd2ab7e08bbbabbe0f0525
大氓 <yiming.jin@live.com>
2017-01-02 06:48:06 -0800

    tidy loong.pm

--- lib/Loong.pm
+++ lib/Loong.pm
@@ -11,38 +11,37 @@ use Loong::UserAgent;
 use Loong::Crawler;
 use Loong::Worker::Loader;
 
-has config => sub { Loong::Config->new };
+has config  => sub { Loong::Config->new };
 has mapping => sub { Loong::Mapping->new };
 has crawler => sub { Loong::Crawler->new };
-has url => sub { Mojo::URL->new($_[0]) };
-has minion => sub { Mojo::Minion->new };
-has log => sub { Mojo::Log->new };
-has ua => sub { Loong::UserAgent };
+has url     => sub { Mojo::URL->new($_[0]) };
+has minion  => sub { Mojo::Minion->new };
+has log     => sub { Mojo::Log->new };
+has ua      => sub {Loong::UserAgent};
 
-my $active = 0;
+my $active   = 0;
 my $max_conn = 10;
-my $xxx=0
-;
+my $xxx      = 0;
 
-sub new{
+sub new {
     my $self = shift->SUPER::new(@_);
 }
 
-sub invoke_task{
-    my ( $self, $seed ) = @_;
+sub invoke_task {
+    my ($self, $seed) = @_;
 
     Mojo::IOLoop->recurring(
         0 => sub {
-            for ( $active + 1 .. $max_conn ) {
-                my $item = $worker->register->dequeue( 0, queue => $seed );
-                return ( $active or Mojo::IOLoop->stop )
+            for ($active + 1 .. $max_conn) {
+                my $item = $worker->register->dequeue(0, queue => $seed);
+                return ($active or Mojo::IOLoop->stop)
                   if !ref($item) || !$item->{url};
 
                 ++$active;
                 my $gc = Loong::Crawler->new(
                     active       => $active,
                     url          => $item->{url},
-                    extra_config => { %$item, %{ $self->config->{$seed} } }
+                    extra_config => {%$item, %{$self->config->{$seed}}}
                 );
                 $gc->run;
             }
@@ -51,7 +50,7 @@ sub invoke_task{
     Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
 }
 
-sub fuck{
+sub fuck {
 }
 
 1;
