2c51d440cf4acf0b070787437584a1dbce2d65da
大氓 <yiming.jin@live.com>
2017-02-14 14:57:26 +0000

    log level

---
+++ lib/Loong/Scraper/Caoliu.pm
@@ -0,0 +1,19 @@
+package Loong::Scraper::Caoliu;
+
+use Loong::Scraper -route;
+use utf8;
+
+
+get '/htm_data/\d+/\d+/\d+.html' => sub {
+    my ($self,$dom,$ctx) = @_;
+
+    my $tip_top = $dom->at('table > tbody > tr > td > h4');
+    my $ret = { next => [], data => {} };
+    if("$tip_top"=~ m{^.\w+/\d+MB.(\w+-\d+)\s*.+?\[\S+\]\[\S+\]}){
+        my $fanhao = $1;
+        warn "fanhao is $fanhao\n";
+    }
+    return {};
+};
+
+1;
